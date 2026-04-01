033e0a02ee30d2fa91f8d61cdc3bd338ae9203d7
sahaj <meghabharti101@gmail.com>
2026-04-02 01:52:40 +0530

    fix render proxy for rate limit

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.set('trust proxy', 1);
 
 const PORT = process.env.PORT || 3000;
 const ADMIN_KEY = process.env.ADMIN_KEY;
